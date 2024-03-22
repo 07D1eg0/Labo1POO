87b7795be098ccc83c88e51523c819231bacc6a5
07D1eg0 <00085720@uca.edu.sv>
2024-03-22 10:12:15 -0600

    Añadiendo funciones calc

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Miltiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("5. Salir");
+            System.out.print("Ingrese la opcion que desea utilizar: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    Suma(scanner);
+                    break;
+                case 2:
+                    Resta(scanner);
+                    break;
+                case 3:
+                    Multi(scanner);
+                    break;
+                case 4:
+                    Division(scanner);
+                    break;
+                case 5:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        } while (opcion != 5);
+
+        scanner.close();
+    }
+
+    public static void Suma(Scanner scanner) {
+        System.out.print("Ingresa el primer numero: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingresa el segundo numero: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 + num2;
+        System.out.println("Respuesta:  " + result);
+    }
+
+    public static void Resta(Scanner scanner) {
+        System.out.print("Ingresa el primer numero: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingresa el segundo numero: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 - num2;
+        System.out.println("Respuesta:  " + result);
+    }
+
+    public static void Multi(Scanner scanner) {
+        System.out.print("Ingresa el primer numero: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingresa el segundo numero: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 * num2;
+        System.out.println("Respuesta: " + result);
+    }
+
+    public static void Division(Scanner scanner) {
+        System.out.print("Ingresa el dividiendo: ");
+        double dividend = scanner.nextDouble();
+        System.out.print("Ingresa el divisor: ");
+        double divisor = scanner.nextDouble();
+
+        if (divisor == 0) {
+            System.out.println("Error. Division de 0");
+        } else {
+            double result = dividend / divisor;
+            System.out.println("Respuesta: " + result);
         }
     }
 }
